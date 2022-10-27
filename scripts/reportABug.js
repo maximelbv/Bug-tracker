e26e876227b568df5f94019708e9c3a372fb55ae
Your Name <lefebvremaxime00@gmail.com>
2022-10-27 10:23:59 +0200

    finish add bug function + anim & redirection

--- scripts/reportABug.js
+++ scripts/reportABug.js
@@ -36,5 +36,61 @@ async function logout() {
 
 }
 
+async function addReport(e) {
+
+    e.preventDefault();
+
+    console.log('ok');
+
+    const token = sessionStorage.getItem('token');
+    const userId = sessionStorage.getItem('userId');
+
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('desc').value;
+    console.log(token, userId)
+
+    try {
+        const response = await fetch(`${api}/add/${token}/${userId}`, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                title: title,
+                description: description,
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        if (result.result.status === 'failure') {
+            document.querySelector('.errorCtn').innerHTML = `<p class='errorMsg'>${result.result.message}</p>`
+        }
+
+        if (result.result.status === 'done') {
+            console.log(result.result);
+            console.log('done');
+            document.querySelector('.main').innerHTML = 
+            `
+            <div class='animCtn'>
+                <lottie-player 
+                    class='validationAnim' 
+                    src="./media/anim/validation.json" 
+                    background="transparent" 
+                    speed="1"
+                    autoplay
+                ></lottie-player>
+            </div>
+            `
+            setTimeout(() => {location.replace('index.html')}, 1500);
+        }
+
+    }
+    catch (err) { console.log(err) }
+}
+
 logoutBtn.addEventListener('click', logout);
 returnHomeBtn.addEventListener('click', () => {location.replace('index.html')});
+document.querySelector('.reportForm').addEventListener('submit', addReport);
